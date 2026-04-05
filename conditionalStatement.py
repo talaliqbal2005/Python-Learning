e39c9ccf302e800a1d8029e1a661e075831b040e
Talal iqbal <iqbaltalal2005@gmail.com>
2026-04-06 00:10:33 +0500

    comments added

--- conditionalStatement.py
+++ conditionalStatement.py
@@ -1,42 +1,44 @@
-# a = "talal"
-# b = "iqbal"
+# printing length
+a = "talal"
+b = "iqbal"
 
-# print(len(a))
-# print(len(b))
+print(len(a))
+print(len(b))
 
-# c = "talal" + " "+ "iqbal"
-# print(len(c))
+# length count including wide space
+c = "talal" + " "+ "iqbal"
+print(len(c))
+                                            
+                                            # Conditional Statement
 
-# print(a[0 : 2])
+#traffic light program
+light =input("enter the light : ")
 
-# print(a.endswith("o"))
-# print(a.count("a"))
+if(light == "red"):
+    print("STOP")
 
-# light =input("enter the light : ")
+elif(light == "yellow"):
+    print("START")
 
-# if(light == "red"):
-#     print("STOP")
+else:
+    print("GO")
 
-# elif(light == "yellow"):
-#     print("START")
+#Students grades on basis of marks
+marks = int(input("enter your marks : "))
 
-# else:
-#     print("GO")
+if(marks >= 90 and marks <= 100):
+    print("grade A")
+elif(marks >=80 and marks < 90):
+    print(" Grade B")
+elif( marks >= 70 and marks < 80):
+    print("Grade c")
+elif( marks >= 60 and marks < 70):
+    print("Grade D")
+else:
+    print(" you are failed re atempt ")
+print("the programs end ... THANKYOU")
 
-# marks = int(input("enter your marks : "))
-
-# if(marks >= 90 and marks <= 100):
-#     print("grade A")
-# elif(marks >=80 and marks < 90):
-#     print(" Grade B")
-# elif( marks >= 70 and marks < 80):
-#     print("Grade c")
-# elif( marks >= 60 and marks < 70):
-#     print("Grade D")
-# else:
-#     print(" you are failed re atempt ")
-# print("the programs end ... THANKYOU")
-
+#find the greatest of all
 a = int(input("enter the first number : "))
 b = int(input("enter the second number : "))
 c = int(input("enter the third number : "))
